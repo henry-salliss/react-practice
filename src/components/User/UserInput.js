95d312e57f22c984552d2049e69044e2381f898e
Henry Salliss <henrysalliss@gmail.com>
2021-10-14 19:48:19 +0100

    added state to store entered values in UserInput component

--- src/components/User/UserInput.js
+++ src/components/User/UserInput.js
@@ -1,20 +1,33 @@
 import React from "react";
+import { useState } from "react";
 import Card from "../UI/Card";
 import styles from "./UserInput.module.css";
 import Button from "../UI/Button";
 
 const UserInput = function () {
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
+
   const submitHandler = function (e) {
     e.preventDefault();
+    console.log(name, age);
+  };
+
+  const nameChange = function (e) {
+    setName(e.target.value);
+  };
+
+  const ageChange = function (e) {
+    setAge(e.target.value);
   };
 
   return (
     <Card className={styles.input}>
       <form onSubmit={submitHandler}>
         <label htmlFor="name">Name</label>
-        <input id="name"></input>
+        <input id="name" onChange={nameChange}></input>
         <label htmlFor="age">Age</label>
-        <input id="age"></input>
+        <input id="age" onChange={ageChange}></input>
         <Button type="submit">Submit</Button>
       </form>
     </Card>
