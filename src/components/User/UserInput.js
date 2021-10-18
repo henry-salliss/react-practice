fe9db75f59db3d5477c970d4949d637346b4e4b4
Henry Salliss <henrysalliss@gmail.com>
2021-10-18 15:05:14 +0100

    added error modal component and added state management to handle errors and render modal only when needed

--- src/components/User/UserInput.js
+++ src/components/User/UserInput.js
@@ -3,17 +3,30 @@ import { useState } from "react";
 import Card from "../UI/Card";
 import styles from "./UserInput.module.css";
 import Button from "../UI/Button";
+import ErrorModal from "../UI/ErrorModal";
 
 const UserInput = function (props) {
   const [name, setName] = useState("");
   const [age, setAge] = useState("");
+  const [error, setError] = useState();
 
   const submitHandler = function (e) {
     e.preventDefault();
 
     // return on bad input
-    if (name.trim().length === 0 || age.trim().length === 0) return;
-    if (+age < 1) return;
+    if (name.trim().length === 0 || age.trim().length === 0) {
+      setError({
+        title: "Please input info",
+        message: "You need to type your info into the inputs",
+      });
+      return;
+    }
+    if (+age < 1) {
+      setError({
+        title: "Invalid age input",
+        message: "Your age needs to be 1 or over",
+      });
+    }
 
     // reset values
     props.onSaveData(name, age);
@@ -29,16 +42,31 @@ const UserInput = function (props) {
     setAge(e.target.value);
   };
 
+  const errorHandler = () => {
+    setError(null);
+  };
+
   return (
-    <Card className={styles.input}>
-      <form onSubmit={submitHandler} autoComplete="off">
-        <label htmlFor="name">Name</label>
-        <input id="name" onChange={nameChange} value={name}></input>
-        <label htmlFor="age">Age</label>
-        <input id="age" onChange={ageChange} value={age}></input>
-        <Button type="submit">Submit</Button>
-      </form>
-    </Card>
+    <div>
+      {error ? (
+        <ErrorModal
+          title={error.title}
+          message={error.message}
+          onRemoveError={errorHandler}
+        />
+      ) : (
+        ""
+      )}
+      <Card className={styles.input}>
+        <form onSubmit={submitHandler} autoComplete="off">
+          <label htmlFor="name">Name</label>
+          <input id="name" onChange={nameChange} value={name}></input>
+          <label htmlFor="age">Age</label>
+          <input id="age" onChange={ageChange} value={age}></input>
+          <Button type="submit">Submit</Button>
+        </form>
+      </Card>
+    </div>
   );
 };
 
